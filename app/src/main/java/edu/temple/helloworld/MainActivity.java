1f84bb8d65e8e7bd41e3ea10699443797d70ff88
Steph <stephenwmelnick@gmail.com>
2019-09-14 14:27:30 -0400

    Added button functionality and message!

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //END-BOILERPLATE
+
+        final Button button = findViewById(R.id.button_id);
+        final String buttonPressed = getResources().getString(R.string.button_pressed);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                button.setText(buttonPressed);
+            }
+        });
     }
 }
